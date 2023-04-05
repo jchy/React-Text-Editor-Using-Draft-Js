3c5c5185fa80c97ed33727e66f87e005fae605a1
Programera-Pvt-Ltd <programerapvtltd@gmail.com>
2023-04-05 18:54:33 +0530

    remove: suggestion on typing @

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -93,24 +93,6 @@ export default class TextEditor extends Component {
                 wrapperClassName="wrapperClassName"
                 editorClassName="editorClassName"
                 onEditorStateChange={this.onEditorStateChange}
-                mention={{
-                  separator: " ",
-                  trigger: "@",
-                  suggestions: [
-                    { text: "APPLE", value: "apple" },
-                    { text: "BANANA", value: "banana", url: "banana" },
-                    { text: "CHERRY", value: "cherry", url: "cherry" },
-                    { text: "DURIAN", value: "durian", url: "durian" },
-                    { text: "EGGFRUIT", value: "eggfruit", url: "eggfruit" },
-                    { text: "FIG", value: "fig", url: "fig" },
-                    {
-                      text: "GRAPEFRUIT",
-                      value: "grapefruit",
-                      url: "grapefruit",
-                    },
-                    { text: "HONEYDEW", value: "honeydew", url: "honeydew" },
-                  ],
-                }}
               />
             </div>
             <div style={{ textAlign: "left", marginTop: "10px" }}>
